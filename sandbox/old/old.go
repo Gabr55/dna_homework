868649b5fa517dea2c9dcb1e109e74b93992b421
Your Name <you@example.com>
2024-03-05 22:47:47 +0300

    files replace

--- sandbox/old/old.go
+++ sandbox/old/old.go
@@ -1,34 +1,42 @@
-package dna
+package main
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"unicode"
 )
 
-// Заюзать buffio
-
 func Compress(source string) (compressed string, err error) {
+	// Проверка на пустую строку
 	if len(source) == 0 {
 		return "", nil
 	}
 
+	// Объявляем string builder
 	var builder strings.Builder
+	// Предыдущий символ
 	var previousChar rune
+	// Количество символов
 	var countChar int
 
+	// Проход по символам в строке
 	for _, char := range source {
 		if char != previousChar {
+			// Проеверяем, что он не первый и записываем в билдер символ и количество
 			if previousChar != 0 {
 				builder.WriteString(string(previousChar))
-				builder.WriteString(strconv.Itoa(countChar))
+				builder.WriteString(strconv.Itoa(countChar)) // Конвертриуем int в string
 			}
 			previousChar = char
 			countChar = 1
 		} else {
+			// Если новый символ совпадает с предыдущим - увеличиваем количество
 			countChar++
 		}
 	}
+
+	// Добавляем последний символ и его количество
 	builder.WriteString(string(previousChar))
 	builder.WriteString(strconv.Itoa(countChar))
 
@@ -36,12 +44,17 @@ func Compress(source string) (compressed string, err error) {
 }
 
 func Decompress(compressed string) (source string, err error) {
+	// Объявляем String Builder
 	var sourceString strings.Builder
+	// Текущий символ
 	var currentChar rune
+	// Общий счетчик
 	var count int = 0
 
+	// Проход по символам в строке
 	for _, char := range compressed {
 		if char != 0 {
+			// Для каждой четной итерации записываем символ
 			if count%2 == 0 {
 				currentChar = char
 				sourceString.WriteString(string(char))
@@ -53,6 +66,7 @@ func Decompress(compressed string) (source string, err error) {
 				if err != nil {
 					panic("Неподдерживаемая строка")
 				}
+				// Заполняем оставшееся количество символов
 				for i := 0; i < count-1; i++ {
 					sourceString.WriteString(string(currentChar))
 				}
@@ -61,4 +75,9 @@ func Decompress(compressed string) (source string, err error) {
 		}
 	}
 	return sourceString.String(), nil
+}
+
+func main() {
+	fmt.Println(Compress("aaaabbсaa"))
+	fmt.Println(Decompress("a4b2с1a2"))
 }
